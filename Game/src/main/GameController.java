7c7471fc335561741e2268e12ede4d6e4a116e55
Lautaro <lautarodebenedetti@gmail.com>
2018-04-13 23:25:32 -0300

    actualizar
    actualizaciones por segundos, frames por segundos

---
+++ Game/src/main/GameController.java
@@ -0,0 +1,69 @@
+package main;
+
+import Dibujador.VentanaJuego;
+
+public class GameController implements Runnable{
+	private static VentanaJuego ventanaJuego;
+	private static volatile Thread thread;		//agregamos el volatile porque estamos usando 2 threads
+	private static boolean enFuncionamiento = false;
+	private static int aps = 0; //actualizaciones por segundo
+	private static int fps = 0; //frames por segundo
+	
+	public GameController(){
+		ventanaJuego = new VentanaJuego();
+	}
+	//synchronized permite que no se puedan ejecutar al mismo tiempo
+	public synchronized void iniciar(){
+		enFuncionamiento = true;
+		thread = new Thread(this,"Graficos");
+		thread.start();
+	}
+	
+	public synchronized void detener(){
+		enFuncionamiento = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void actualizar(){
+		aps++;
+	}
+	
+	public void mostrar(){
+		fps++;
+	}
+	//utilizamos nanosegundos, para evitar conflictos si se ejecuta en otro sistema operativo
+	public void run() {														//actualiza el avance y dibujos del juego 
+		final int NS_POR_SEGUNDO = 1000000000; 								//cantidad de nanosegundos equivalentes a un segundo
+		final byte APS_OBJETIVO = 60;										//cuantas actualizaciones queremos por segundo (60)
+		final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO; 	//cuantos nanosegundos transcurren por actualizacion
+		
+		long referenciaActualizacion = System.nanoTime();	//se almacena una cantidad exacta de nanosegundos, justo en el momento que se ejecuta
+		long referenciaContador = System.nanoTime();		//para contar los frames (EXTRA)
+		
+		double tiempoTranscurrido;
+		double delta = 0;
+		while(enFuncionamiento){
+			final long inicioBucle = System.nanoTime();		//tomamos la cantidad exacta de nanosegundos cuando comienza el bucle
+			tiempoTranscurrido = inicioBucle - referenciaActualizacion;//tomamos el tiempo transcurrido de cada ciclo
+			referenciaActualizacion = inicioBucle;
+			
+			delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;	//sumamos a delta el tiempo trascurrido del ciclo dividido por los nanosegundos
+			while (delta >= 1){			//una vez que delta ya cumplio en llegar a uno de los bytes de APS_OBJETIVO, el juego se actualizara
+				actualizar();
+				delta--;
+			}
+			mostrar();
+			
+			if(System.nanoTime() - referenciaContador > NS_POR_SEGUNDO){//esto hace que el contador se actualice cada segundo.
+				System.out.println("APS: " + aps + " || FPS: " + fps);
+				aps = 0;
+				fps = 0;
+				referenciaContador = System.nanoTime();
+			}
+		}
+	}
+}
---
+++ Game/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GameController juego = new GameController();
+		juego.iniciar();
+	}
+
+}
