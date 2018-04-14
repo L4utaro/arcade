06deaa1f5ece2b67c8c1206c03f0c4f8fa7860e2
Lautaro <lautarodebenedetti@gmail.com>
2018-04-14 19:31:04 -0300

    Pantalla, graficos

--- Game/src/control/Teclado.java
+++ Game/src/control/Teclado.java
@@ -3,33 +3,36 @@ package control;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Teclado implements KeyListener{
+public class Teclado implements KeyListener {
 	private final static int numeroTeclas = 120;
-	private final boolean [] teclas = new boolean[numeroTeclas];
-	
+	private final boolean[] teclas = new boolean[numeroTeclas];
+
 	public boolean w;
 	public boolean s;
 	public boolean a;
 	public boolean d;
 	public boolean space;
-	
+
 	public void actualizar() {
-		w = teclas[KeyEvent.VK_W]; //la tecla arriba equivale a pulsar W
-		s = teclas[KeyEvent.VK_S]; //la tecla arriba equivale a pulsar S
-		a = teclas[KeyEvent.VK_A]; //la tecla arriba equivale a pulsar A
-		d = teclas[KeyEvent.VK_D]; //la tecla arriba equivale a pulsar D
+		w = teclas[KeyEvent.VK_W]; // la tecla arriba equivale a pulsar W
+		s = teclas[KeyEvent.VK_S]; // la tecla arriba equivale a pulsar S
+		a = teclas[KeyEvent.VK_A]; // la tecla arriba equivale a pulsar A
+		d = teclas[KeyEvent.VK_D]; // la tecla arriba equivale a pulsar D
 	}
-	//HAY QUE ARREGLAR ESTA PARTE, NO HACE LAS ACCIONES DE DEBAJO
-	//teclas que se mantienen pulsada
+
+	// HAY QUE ARREGLAR ESTA PARTE, NO HACE LAS ACCIONES DE DEBAJO
+	// teclas que se mantienen pulsada
 	public void keyPressed(KeyEvent e) {
-		teclas[e.getKeyCode()] = true;		//pone en true la tecla pulsada
+		teclas[e.getKeyCode()] = true; // pone en true la tecla pulsada
 	}
-	//teclas que se sueltan
+
+	// teclas que se sueltan
 	public void keyReleased(KeyEvent e) {
-		teclas[e.getKeyCode()] = false;		//pone en false tecla soltada
-		
+		teclas[e.getKeyCode()] = false; // pone en false tecla soltada
+
 	}
-	//pulsar y soltar
+
+	// pulsar y soltar
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
--- Game/src/main/Juego.java
+++ Game/src/main/Juego.java
@@ -3,27 +3,37 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
 import control.Teclado;
 //import dibujador.VentanaJuego;
+import grafico.Pantalla;
 
-public class Juego extends Canvas implements Runnable{
-	//private static VentanaJuego ventanaJuego;
-	private static volatile Thread thread;		//agregamos el volatile porque estamos usando 2 threads
+public class Juego extends Canvas implements Runnable {
+	// private static VentanaJuego ventanaJuego;
+	private static volatile Thread thread; // agregamos el volatile porque estamos usando 2 threads
 	private static boolean enFuncionamiento = false;
-	private static int aps = 0; //actualizaciones por segundo
-	private static int fps = 0; //frames por segundo
+	private static int aps = 0; // actualizaciones por segundo
+	private static int fps = 0; // frames por segundo
 	private static Teclado teclado;
+	private static int x = 0;
+	private static int y = 0;
+	private static Pantalla pantalla;
+	private static BufferedImage imagen = new BufferedImage(Constantes.ANCHO, Constantes.ALTO,
+			BufferedImage.TYPE_INT_RGB);
+	private static int [] pixeles = ((DataBufferInt) imagen.getRaster().getDataBuffer()).getData();
 	
-	//ventanaJuego
+	// ventanaJuego
 	private static final long serialVersionUID = 1L;
 	private static JFrame ventana;
-	//ventanaJuego
-	
-	public Juego(){
-		//ventanaJuego
+	// ventanaJuego
+
+	public Juego() {
+		// ventanaJuego
 		setPreferredSize(new Dimension(Constantes.ANCHO, Constantes.ALTO));
 		ventana = new JFrame(Constantes.NOMBREJUEGO);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,21 +43,23 @@ public class Juego extends Canvas implements Runnable{
 		ventana.pack();
 		ventana.setLocationRelativeTo(null);
 		ventana.setVisible(true);
-		//ventanaJuego
+		// ventanaJuego
 		
+		pantalla = new Pantalla(Constantes.ANCHO, Constantes.ALTO);
 		teclado = new Teclado();
 		addKeyListener(teclado);
 		setFocusable(true);
-		//ventanaJuego = new VentanaJuego();
+		// ventanaJuego = new VentanaJuego();
 	}
-	//synchronized permite que no se puedan ejecutar al mismo tiempo
-	public synchronized void iniciar(){
+
+	// synchronized permite que no se puedan ejecutar al mismo tiempo
+	public synchronized void iniciar() {
 		enFuncionamiento = true;
-		thread = new Thread(this,"Graficos");
+		thread = new Thread(this, "Graficos");
 		thread.start();
 	}
-	
-	public synchronized void detener(){
+
+	public synchronized void detener() {
 		enFuncionamiento = false;
 		try {
 			thread.join();
@@ -55,54 +67,56 @@ public class Juego extends Canvas implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
-	public void actualizar(){
+
+	public void actualizar() {
 		teclado.actualizar();
-		if(teclado.w) {
-			System.out.println("arriba");
-		}
-		if(teclado.s) {
-			System.out.println("abajo");
-		}
-		if(teclado.a) {
-			System.out.println("izquierda");
-		}
-		if(teclado.d) {
-			System.out.println("derecha");
-		}
 		aps++;
 	}
-	
-	public void mostrar(){
+
+	public void mostrar() {
+		BufferStrategy estrategia = getBufferStrategy();
+		if(estrategia == null) {
+			createBufferStrategy(3);
+			return;
+		}
 		fps++;
 	}
-	//utilizamos nanosegundos, para evitar conflictos si se ejecuta en otro sistema operativo
-	public void run() {														//actualiza el avance y dibujos del juego 
-		final int NS_POR_SEGUNDO = 1000000000; 								//cantidad de nanosegundos equivalentes a un segundo
-		final byte APS_OBJETIVO = 60;										//cuantas actualizaciones queremos por segundo (60)
-		final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO; 	//cuantos nanosegundos transcurren por actualizacion
-		
-		long referenciaActualizacion = System.nanoTime();	//se almacena una cantidad exacta de nanosegundos, justo en el momento que se ejecuta
-		long referenciaContador = System.nanoTime();		//para contar los frames (EXTRA)
-		
+
+	// utilizamos nanosegundos, para evitar conflictos si se ejecuta en otro sistema
+	// operativo
+	public void run() { // actualiza el avance y dibujos del juego
+		final int NS_POR_SEGUNDO = 1000000000; // cantidad de nanosegundos equivalentes a un segundo
+		final byte APS_OBJETIVO = 60; // cuantas actualizaciones queremos por segundo (60)
+		final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO; // cuantos nanosegundos transcurren por
+																			// actualizacion
+
+		long referenciaActualizacion = System.nanoTime(); // se almacena una cantidad exacta de nanosegundos, justo en
+															// el momento que se ejecuta
+		long referenciaContador = System.nanoTime(); // para contar los frames (EXTRA)
+
 		double tiempoTranscurrido;
 		double delta = 0;
-		
-		requestFocus();//saltea tener que hacer el clik en pantalla. (osea, podes tocar teclas cuando se inicia)
-		
-		while(enFuncionamiento){
-			final long inicioBucle = System.nanoTime();		//tomamos la cantidad exacta de nanosegundos cuando comienza el bucle
-			tiempoTranscurrido = inicioBucle - referenciaActualizacion;//tomamos el tiempo transcurrido de cada ciclo
+
+		requestFocus();// saltea tener que hacer el clik en pantalla. (osea, podes tocar teclas cuando
+						// se inicia)
+
+		while (enFuncionamiento) {
+			final long inicioBucle = System.nanoTime(); // tomamos la cantidad exacta de nanosegundos cuando comienza el
+														// bucle
+			tiempoTranscurrido = inicioBucle - referenciaActualizacion;// tomamos el tiempo transcurrido de cada ciclo
 			referenciaActualizacion = inicioBucle;
-			
-			delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;	//sumamos a delta el tiempo trascurrido del ciclo dividido por los nanosegundos
-			while (delta >= 1){			//una vez que delta ya cumplio en llegar a uno de los bytes de APS_OBJETIVO, el juego se actualizara
+
+			delta += tiempoTranscurrido / NS_POR_ACTUALIZACION; // sumamos a delta el tiempo trascurrido del ciclo
+																// dividido por los nanosegundos
+			while (delta >= 1) { // una vez que delta ya cumplio en llegar a uno de los bytes de APS_OBJETIVO, el
+									// juego se actualizara
 				actualizar();
 				delta--;
 			}
 			mostrar();
-			
-			if(System.nanoTime() - referenciaContador > NS_POR_SEGUNDO){//esto hace que el contador se actualice cada segundo.
+
+			if (System.nanoTime() - referenciaContador > NS_POR_SEGUNDO) {// esto hace que el contador se actualice cada
+																			// segundo.
 				System.out.println("APS: " + aps + " || FPS: " + fps);
 				aps = 0;
 				fps = 0;
